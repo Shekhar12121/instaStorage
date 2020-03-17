f14d8f7cf1ea9dc2d1604ebf651bf86d59c4b071
Shekhar12121 <geekshekhar@gmail.com>
2020-03-17 15:22:05 +0530

    pushing all files in repo

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ type: 'json' }))
+
+mongoose.connect("mongodb://instauser:insta1234@ds227035.mlab.com:27035/datagroup", { useNewUrlParser: true },
+function(err){
+  if(err){
+    console.log('errrr',err);
+  }else{
+    console.log('connected to database')
+  }
+})
+
+let instaSchema = new mongoose.Schema({
+  username:String,
+  userpass:String
+});
+
+
+let InstaModel = mongoose.model('InstaModel', instaSchema);
+
+
+
+
+//app.use(express.static(path.join(__dirname, 'dist/insta')));
+
+app.post('/dataSend', function (req, res) {
+  console.log(req.body);
+  InstaModel.create({
+    username:req.body.username,
+    userpass:req.body.userpass
+  },(err,data)=>{
+    if(err){
+      console.log('error in model', err)
+    }else{
+      console.log('models Data', data)
+    }
+    
+  })
+ })
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist/insta/index.html'))
+ })
+
+
+
+const port = process.env.PORT || 4600
+
+app.listen(port, function () {
+  console.log(`CORS-enabled web server listening on port ${port}`)
+})
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  logForm(e){
+     console.log('dd',e);
+
+
+    this.http.post('http://localhost:4600/dataSend', e).subscribe(res=>{
+      console.log('hh',res)
+    },
+    err => console.log('err',err))
+
+  }
+
+}
